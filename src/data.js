6e67a3f144e77da4cf236297f799bc845742a8ec
ahodzic11 <ahodzic11@etf.unsa.ba>
2022-09-11 01:24:20 +0200

    Navbar and announcement features completed.

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://www.ultimate-guitar.com/static/article/draft/134837_kUqn4_yryXVEZDMG_61358.jpg",
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://cdn.mos.cms.futurecdn.net/nbyjQ5giJsM4G9Hv7N6DZS.jpg",
+        title: "WINTER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fcf1ed",
+    },
+    {
+        id: 3,
+        img: "https://img.audiofanzine.com/images/u/product/normal/gibson-les-paul-classic-2465.jpg",
+        title: "POPULAR SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fbf0f4",
+    }
+]
