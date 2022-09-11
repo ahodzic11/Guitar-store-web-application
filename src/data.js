4773faf123126921de1b451afdc7a56b3fee170f
ahodzic11 <ahodzic11@etf.unsa.ba>
2022-09-11 21:12:21 +0200

    Added products page

--- src/data.js
+++ src/data.js
@@ -21,3 +21,48 @@ export const sliderItems = [
         bg: "fbf0f4",
     }
 ]
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://www.ultimate-guitar.com/static/article/draft/134837_kUqn4_yryXVEZDMG_61358.jpg",
+        title: "LES PAUL",
+    },
+    {
+        id: 2,
+        img: "https://www.fmicassets.com/Damroot/FacebookJpg/10001/0144522500_gtr_frt_001_rr.jpg",
+        title: "STRATOCASTER",
+    },
+    {
+        id: 3,
+        img: "https://www.musicinstrumentnews.co.uk/wp-content/uploads/2022/02/Fender_GH_RWTele_Top.jpg",
+        title: "TELECASTER",
+    },
+]
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: "https://www.acme.eu/sites/default/files/styles/max_1000x1000/public/field_images/2021-05/3_3_6_8_5_2_839f91f3f8190271eb2f9be9729f86d9_original.tif.png?itok=o8HHjG7s",
+    },
+    {
+        id: 2,
+        img: "https://www.tradeinn.com/f/13737/137370580/apple-airpods-pro-wireless-headphones.jpg",
+    },
+    {
+        id: 3,
+        img: "https://static.imtec.ba/191010-large_default/canon-eos-rp-rf24-105mm.jpg",
+    },
+    {
+        id: 4,
+        img: "https://www.nikon.hr/imported/images/web/EU/products/digital-cameras/dslr/d5600/d5600_afp_18_55_vr_front_big--original.png",
+    },
+    {
+        id: 5,
+        img: "https://cdn.shopify.com/s/files/1/0301/2263/9499/products/CX88500_SEL2470GM_right.jpg?v=1658210868",
+    },
+    {
+        id: 6,
+        img: "https://img10.jd.co.th/n0/jfs/t10/227/5238452115/94525/f5be29df/602e069dN991e9665.jpg!q70.jpg",
+    },
+]
